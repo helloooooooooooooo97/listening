9aeaad2b21c82cd8e8212dafbd56ad93a5428c76
97 <348876414@qq.com>
2026-06-05 21:46:39 +0800

    Initial Commit

---
+++ english/englishApp.swift
@@ -0,0 +1,17 @@
+//
+//  englishApp.swift
+//  english
+//
+//  Created by apple1 on 2026/6/5.
+//
+
+import SwiftUI
+
+@main
+struct englishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
